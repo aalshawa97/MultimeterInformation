7d08b352096a5525a59b279b6b50d03ce2199a80
Abdullah Mutaz Alshawa <abdullahalshawa@gmail.com>
2025-09-16 15:56:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,23 @@ root = Tk()
 root.title("Welcome!")
 
 def showImage():
-    img = PhotoImage(file=r"MULTIMETER IMAGE FILE PATH HERE")
+    img = PhotoImage(file=r"MULTIMETER IMAGE PATH HERE")
     button = Button(root, image=img)
     button.img = img  # Keep a reference!
     button.grid()
 
 def switch(menu):
-    if menu == "1":
+    if menu == "Yes" or "yes":
         label = Label(root, text="Multimeters measure electrical properties such as voltage, current, and resistance.\n"
                                  "Current is a measure of the rate at which electric charge flows past a specific point in a circuit.\n"
                                  "Resistance is a measure of how much the flow of electrons is slowed down, measured in ohms.\n"
                                  "Voltage is the measure of electrical potential difference that pushes electrons through a circuit.\n"
                                  "V = I(current) * R(resistance)")
         label.grid()
-    else:
         showImage()
 
 # GUI-based input instead of console
-label = Label(root, text="Press 1 if you would like to learn about multimeters. Press 2 if you would like see an image of a multimeter. Press the X in the top right corner to exit.",)
+label = Label(root, text="Enter yes if you would like to learn about multimeters. Press the X in the top right corner to exit.",)
 label.grid()
 
 entry = Entry(root)
